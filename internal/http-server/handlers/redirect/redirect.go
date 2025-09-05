01f90eebdeb6d310b424dbd34c50e08c0e1ccec0
kiribu <kiryshabutor@gmail.com>
2025-09-05 18:15:08 +0300

    add cmd/url-shortener as normal folder

---
+++ internal/http-server/handlers/redirect/redirect.go
@@ -0,0 +1,61 @@
+package redirect
+
+import (
+	"errors"
+	"log/slog"
+	"net/http"
+
+	resp "url-shortener/internal/lib/api/response"
+	"url-shortener/internal/lib/logger/sl"
+	"url-shortener/internal/storage"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/render"
+)
+
+type URLGeter interface {
+	GetURL(alias string) (string, error)
+}
+
+func New(log *slog.Logger, urlGeter URLGeter) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		
+		const fn = "handlers.url.redirect.New"
+
+		log = log.With(
+			slog.String("fn", fn),
+			slog.String("request_id", middleware.GetReqID(r.Context())),
+		)
+
+		alias := chi.URLParam(r, "alias")
+		if alias == "" {
+			log.Info("alias is empty")
+
+			render.JSON(w, r, resp.Error("invalid request"))
+
+			return
+		}
+
+		resURL, err := urlGeter.GetURL(alias)
+		if errors.Is(err, storage.ErrURLNotFound) {
+			log.Info("url not found", "alias", alias)
+
+			render.JSON(w, r, resp.Error("not found"))
+
+			return
+		}
+		if err != nil {
+			log.Info("failed to get url", sl.Err(err))
+
+			render.JSON(w, r, resp.Error("internal error"))
+
+			return
+		}
+
+		log.Info("got url", slog.String("url", resURL))
+
+		http.Redirect(w, r, resURL, http.StatusFound)
+	}
+}
---
+++ internal/http-server/handlers/url/remove/remove.go
@@ -0,0 +1,59 @@
+package remove
+
+import (
+	"errors"
+	"log/slog"
+	"net/http"
+
+	resp "url-shortener/internal/lib/api/response"
+	"url-shortener/internal/lib/logger/sl"
+	"url-shortener/internal/storage"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/render"
+)
+
+type URLDeleter interface {
+	DeleteURL(alias string) error
+}
+
+func New(log *slog.Logger, urlDeleter URLDeleter) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		const fn = "handlers.url.delete.New"
+
+		log = log.With(
+			slog.String("fn", fn),
+			slog.String("request_id", middleware.GetReqID(r.Context())),
+		)
+
+		alias := chi.URLParam(r, "alias")
+		if alias == "" {
+			log.Info("alias is empty")
+
+			render.JSON(w, r, resp.Error("invalid request"))
+
+			return
+		}
+
+		err := urlDeleter.DeleteURL(alias)
+		if errors.Is(err, storage.ErrURLNotFound) {
+			log.Info("url not found", "alias", alias)
+
+			render.JSON(w, r, resp.Error("not found"))
+
+			return
+		}
+		if err != nil {
+			log.Info("failed to delete url", sl.Err(err))
+
+			render.JSON(w, r, resp.Error("internal error"))
+
+			return
+		}
+
+		log.Info("delete url", slog.String("alias", alias))
+
+		render.JSON(w, r, resp.OK())
+	}
+}
